ba11ac4688cb21afdccd9af4fd0ca0400669a7d3
Jeff Webb <webbwear@icloud.com>
2018-10-04 07:50:33 -0400

    Core Knowledge logos set

--- src/components/services.js
+++ src/components/services.js
@@ -1,38 +1,46 @@
 import React from "react";
 import ScrollableAnchor from "react-scrollable-anchor";
+// import Logo from '../../public/img/js.png' Ask Mark or Jason about this
 const ServicesOffered = [
   {
-    title: "Responsive",
-    description: "Looks great on any screen size!",
-    icon: "icon-screen-smartphone"
+    title: "HTML5",
+    imageUrl : "/img/html5.png"
   },
   {
-    title: "Redesigned",
-    description: "Freshly redesigned for Bootstrap 4.",
-    icon: "icon-pencil"
+    title: "CSS3",
+    imageUrl: "/img/css3.png"
   },
   {
-    title: "Favorited",
-    description: (
-      <span>
-        Millions of users <i className="fas fa-heart" /> Start Bootstrap!
-      </span>
-    ),
-    icon: "icon-like"
+    title: "JavaScript",
+    imageUrl: "/img/java-script.png"
   },
   {
-    title: "Question",
-    description: "I mustache you a question...",
-    icon: "icon-mustache"
-  }
+    title: "Ruby",
+    imageUrl: "/img/ruby.png"
+  },
+  {
+  title: "React",
+    imageUrl: "/img/react.png"
+  },
+  {
+    title: "Vue",
+    imageUrl: "/img/vue.png"
+  },
+  {
+    title: "GitHub",
+      imageUrl: "/img/git-hub.png"
+    },
+    {
+      title: "WordPress",
+      imageUrl: "/img/word-press.png"
+    }
 ];
 const ServicesSections = () => (
   <ScrollableAnchor id="services">
     <section className="content-section bg-primary text-white text-center">
       <div className="container">
         <div className="content-section-heading">
-          <h3 className="text-secondary mb-0">Services</h3>
-          <h2 className="mb-5">What We Offer</h2>
+          <h2 className="mb-5">Core Knowledge</h2>
         </div>
         <div className="row">
           {ServicesOffered.map((service, index) => (
@@ -40,8 +48,8 @@ const ServicesSections = () => (
               className="col-lg-3 col-md-6 mb-5 mb-lg-0"
               key={`service_${index}`}
             >
-              <span className="service-icon rounded-circle mx-auto mb-3">
-                <i className={service.icon} />
+              <span className="rounded-circle mx-auto mb-3">
+                <img src={service.imageUrl}/>
               </span>
               <h4>
                 <strong>{service.title}</strong>
