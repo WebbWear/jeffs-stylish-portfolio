419b6ece8bc052270e2c4daa00366d2503e9f2a7
Jeff Webb <webbwear@icloud.com>
2018-10-29 14:14:53 -0400

    Porfolio page updated with reactstrap card

--- src/components/callToAction.js
+++ src/components/callToAction.js
@@ -4,7 +4,7 @@ const CallToAction = () => (
   <section className="content-section bg-primary text-white">
     <div className="container text-center">
       <h2 className="mb-4">Download my resume...</h2>
-      <a href="https://docs.google.com/document/d/e/2PACX-1vS38zTOacJU9n_XXhcC-yfi-K2EvG-Nh9-vvYxkOKId5GnGVCszQTcvMX_rrbi6bjYjuQ_8bdJ4vaST/pub" className="btn btn-xl btn-light mr-4">
+      <a href="/img/JeffWebb-resume-2018.pdf" className="btn btn-xl btn-light mr-4">
         Resume
       </a>
       
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,16 +1,52 @@
 import React from "react";
-import ScrollableAnchor from "react-scrollable-anchor";
-const portfolioItems = [
-  {
-    imgSrc: "../img/pcl.png",
-    heading: "Capstone Project",
-    description: "Capstone project from Suncoast Developers Guild"
-  },
-  {
-    imgSrc: "./img/lms.png",
-    heading: "Word Press",
-    description:""
-  }
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+
+  const PortfolioSection = (props) => {
+    return (
+      <div>
+        <Card>
+          <CardImg top width="100%" src="../img/pcl.png" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+        <CardImg top width="100%" src="./img/lms.png" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  };
+
+
+
+
+
+
+
+// import ScrollableAnchor from "react-scrollable-anchor";
+// const portfolioItems = [
+//   {
+//     imgSrc: "../img/pcl.png",
+//     heading: "Capstone Project",
+//     description: "Capstone project from Suncoast Developers Guild",
+//     url: "www.google.com"
+//   },
+//   {
+//     imgSrc: "./img/lms.png",
+//     heading: "Word Press",
+//     description:""
+//   }
   // {
   //   imgSrc: "./img/portfolio-3.jpg",
   //   heading: "Strawberries",
@@ -23,33 +59,34 @@ const portfolioItems = [
   //   description:
   //     "A yellow workspace with some scissors, pencils, and other objects."
   // }
-];
-const PortfolioSection = () => (
-  <ScrollableAnchor id="portfolio">
-    <section className="content-section">
-      <div className="container">
-        <div className="content-section-heading text-center">
-          <h3 className="text-secondary mb-0">Portfolio</h3>
-          <h2 className="mb-5">Recent Projects</h2>
-        </div>
-        <div className="row no-gutters">
-          {portfolioItems.map((project, index) => (
-            <div className="col-lg-6" key={`portfolio_item_${index}`}>
-              <a className="portfolio-item" href="">
-                <span className="caption">
-                  <span className="caption-content">
-                    <h2>{project.heading}</h2>
-                    <p className="mb-0">{project.description}</p>
-                  </span>
-                </span>
-                <img className="img-fluid" src={project.imgSrc} alt="" />
-              </a>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  </ScrollableAnchor>
-);
+// ];
+// const PortfolioSection = () => (
+//   <ScrollableAnchor id="portfolio">
+  
+//     <section className="content-section">
+//       <div className="container">
+//         <div className="content-section-heading text-center">
+//           <h3 className="text-secondary mb-0">Portfolio</h3>
+//           <h2 className="mb-5">Recent Projects</h2>
+//         </div>
+//         <div className="row no-gutters">
+//           {portfolioItems.map((project, index) => (
+//             <div className="col-lg-6" key={`portfolio_item_${index}`}>
+//               <a className="portfolio-item" href="www.google.com">
+//                 <span className="caption">
+//                   <span className="caption-content">
+//                     <h2>{project.heading}</h2>
+//                     <p className="mb-0">{project.description}</p>
+//                   </span>
+//                 </span>
+//                 <img className="img-fluid" src={project.imgSrc} alt="" />
+//               </a>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//    </ScrollableAnchor>
+// );
 
 export default PortfolioSection;
