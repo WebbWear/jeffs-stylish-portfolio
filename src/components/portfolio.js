aaa787708aa43a8312815764bcfa0b8793a45555
Jeff Webb <webbwear@icloud.com>
2018-10-29 15:05:43 -0400

    Add css to porfolio section

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -5,88 +5,27 @@ import { Card, CardImg, CardText, CardBody,
 
   const PortfolioSection = (props) => {
     return (
-      <div>
+      <div className="cards">
         <Card>
           <CardImg top width="100%" src="../img/pcl.png" alt="Card image cap" />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-            <Button>Button</Button>
+            <CardTitle>Capstone Project</CardTitle>
+            <CardSubtitle>Capstone project from Suncoast Developers Guild</CardSubtitle>
+            <CardText>My Capstone project is to solve a communication problem and be able to automated all reminders with my limo company. Using React fronend and Ruby on Rails for the backend. Please alow 30 seconds for servers to fire up.</CardText>
+            <Button a href="http://www.palmaceialimo.com/">Link to site</Button>
           </CardBody>
         </Card>
         <Card>
         <CardImg top width="100%" src="./img/lms.png" alt="Card image cap" />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-            <Button>Button</Button>
+            <CardTitle>WordPress</CardTitle>
+            <CardSubtitle>Launch My Shirt</CardSubtitle>
+            <CardText>Just a simple WordPress site I created to Blog about the shirts I buy.</CardText>
+            <Button a href="https://www.launchmyshirt.com/">Link to site</Button>
           </CardBody>
         </Card>
       </div>
     );
   };
-
-
-
-
-
-
-
-// import ScrollableAnchor from "react-scrollable-anchor";
-// const portfolioItems = [
-//   {
-//     imgSrc: "../img/pcl.png",
-//     heading: "Capstone Project",
-//     description: "Capstone project from Suncoast Developers Guild",
-//     url: "www.google.com"
-//   },
-//   {
-//     imgSrc: "./img/lms.png",
-//     heading: "Word Press",
-//     description:""
-//   }
-  // {
-  //   imgSrc: "./img/portfolio-3.jpg",
-  //   heading: "Strawberries",
-  //   description:
-  //     "Strawberries are such a tasty snack, especially with a little sugar on top!"
-  // },
-  // {
-  //   imgSrc: "./img/portfolio-4.jpg",
-  //   heading: "Workspace",
-  //   description:
-  //     "A yellow workspace with some scissors, pencils, and other objects."
-  // }
-// ];
-// const PortfolioSection = () => (
-//   <ScrollableAnchor id="portfolio">
-  
-//     <section className="content-section">
-//       <div className="container">
-//         <div className="content-section-heading text-center">
-//           <h3 className="text-secondary mb-0">Portfolio</h3>
-//           <h2 className="mb-5">Recent Projects</h2>
-//         </div>
-//         <div className="row no-gutters">
-//           {portfolioItems.map((project, index) => (
-//             <div className="col-lg-6" key={`portfolio_item_${index}`}>
-//               <a className="portfolio-item" href="www.google.com">
-//                 <span className="caption">
-//                   <span className="caption-content">
-//                     <h2>{project.heading}</h2>
-//                     <p className="mb-0">{project.description}</p>
-//                   </span>
-//                 </span>
-//                 <img className="img-fluid" src={project.imgSrc} alt="" />
-//               </a>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//    </ScrollableAnchor>
-// );
 
 export default PortfolioSection;
