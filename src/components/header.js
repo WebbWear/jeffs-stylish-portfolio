c5726c08d69fcacab9307030130fdfaae4b12e31
Jeff Webb <webbwear@icloud.com>
2019-04-01 09:42:27 -0400

    container-fluid on header page

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,11 @@ const Header = () => (
   <header className="masthead d-flex">
     <div className="container text-center my-auto">
       <h1 className="mb-1">portfolio</h1>
-      
+      <div class="container-fluid px-0">
+          "Hi I’m Jeff Webb a 
+            Full stack web developer with
+            a passion for front end design."
+      </div>
       {/* <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about"> */}
       {/* </a> */}
     </div>
